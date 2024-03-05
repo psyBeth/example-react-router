9a873af7a44e9e3c99bdba797aa301132a125bce
Betul <betulkoru06@gmail.com>
2024-03-05 16:11:34 +0300

    personDetail return part, navigate added

--- src/pages/PersonDetail.jsx
+++ src/pages/PersonDetail.jsx
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const PersonDetail = () => {
     const { idx } = useParams()
     const [person, setPerson] = useState([]);
+    const navigate = useNavigate()
 
     const getPerson = () => {
     fetch(`https://reqres.in/api/users/${idx}`)
@@ -17,8 +19,16 @@ const PersonDetail = () => {
 
     console.log(person);
     return (
-        <div>
-            persondetailssreturn
+        <div className="container text-center mt-4">
+            <img className="rounded" src={person?.avatar} alt="img" />
+            <h5>
+                {person?.first_name} {person?.last_name}
+            </h5>
+            <h6>{person?.email}</h6>
+            <div>
+                <button className="btn btn-warning" onClick={() => navigate(-1)}>Back</button>
+                <button className="btn btn-danger" onClick={() => navigate("/")}>Home</button>
+            </div>
         </div>
     )
 }
