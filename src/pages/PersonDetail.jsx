e2ea4143ebf37e0146240f01adffc12396fba4d3
Betul <betulkoru06@gmail.com>
2024-03-05 16:28:55 +0300

    personDetail error handling

--- src/pages/PersonDetail.jsx
+++ src/pages/PersonDetail.jsx
@@ -1,15 +1,25 @@
 import { useParams } from "react-router-dom"
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import NotFound from "./NotFound";
 
 const PersonDetail = () => {
     const { idx } = useParams()
     const [person, setPerson] = useState([]);
     const navigate = useNavigate()
+    const [error, setError] = useState(false);
+    
 
     const getPerson = () => {
     fetch(`https://reqres.in/api/users/${idx}`)
-      .then((res) => res.json())
+        .then((res) => {
+            //? error handling
+            if(!res.ok) {
+                setError(true)
+                throw new Error("Something went wrong :(")
+            }
+            return res.json()
+        })    
       .then((data) => setPerson(data.data))
       .catch((err) => console.log(err));
     };
@@ -18,7 +28,11 @@ const PersonDetail = () => {
     }, []);
 
     console.log(person);
-    return (
+
+    if(error) {
+        return <NotFound />
+    } else {
+        return (
         <div className="container text-center mt-4">
             <img className="rounded" src={person?.avatar} alt="img" />
             <h5>
@@ -30,7 +44,8 @@ const PersonDetail = () => {
                 <button className="btn btn-danger" onClick={() => navigate("/")}>Home</button>
             </div>
         </div>
-    )
+        )
+    }
 }
 
 export default PersonDetail
