6ba620f9d405cdd12f85472818e6b15eb31ff24d
Betul <betulkoru06@gmail.com>
2024-03-05 15:58:58 +0300

    personDetail fetch, states,

--- src/pages/PersonDetail.jsx
+++ src/pages/PersonDetail.jsx
@@ -1,7 +1,26 @@
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
+
 const PersonDetail = () => {
-  return (
-    <div>PersonDetail</div>
-  )
+    const { idx } = useParams()
+    const [person, setPerson] = useState([]);
+
+    const getPerson = () => {
+    fetch(`https://reqres.in/api/users/${idx}`)
+      .then((res) => res.json())
+      .then((data) => setPerson(data.data))
+      .catch((err) => console.log(err));
+    };
+    useEffect(() => {
+        getPerson();
+    }, []);
+
+    console.log(person);
+    return (
+        <div>
+            persondetailssreturn
+        </div>
+    )
 }
 
 export default PersonDetail
