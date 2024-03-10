e8ade175f6ef0daae3f45b06577f4cf1bd3b51b9
Betul <betulkoru06@gmail.com>
2024-03-10 23:10:09 +0300

    private router (nested)

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import FullStack from "./pages/Fullstack";
 import Aws from "./pages/Aws";
 import Frontend from "./pages/Frontend"
 import Backend from "./pages/Backend";
+import Login from "./pages/Login";
+import PrivateRouter from "./pages/PrivateRouter"
 
 function App() {
   return (
@@ -18,8 +20,15 @@ function App() {
     <Nav />
     <Routes>
       <Route path="/" element={<Home />} />
+
+    <Route path="/people" element={<PrivateRouter />}>
       <Route path="/people" element={<People />} />
       <Route path="/people/:idx" element={<PersonDetail />} />
+    </Route>
+
+
+      <Route path="/login" element={<Login />} />
+
       <Route path="/contact" element={<Contact />} />
 
       <Route path="paths" element={<Paths />}>
